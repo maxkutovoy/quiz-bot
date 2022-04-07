778ddc0af2103768b041e9c2be133934e24644af
Max Kutovoy <maxkutovoy@yandex.ru>
2022-04-07 11:37:57 +0600

    Save last asked tg-users question to db.

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,7 @@
 import logging
+import random
 
+import redis
 import requests
 import telegram
 from environs import Env
@@ -10,46 +12,97 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
 logger = logging.getLogger('TG logger')
 
 
+custom_keyboard = [
+    ['Новый вопрос', 'Сдаться'],
+    ['Мой счет']
+]
+
+reply_markup = telegram.ReplyKeyboardMarkup(
+    custom_keyboard,
+    resize_keyboard=True
+)
+
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('Привет! Давай начнем')
 
 
 def tg_send_answer(update: Update, context: CallbackContext):
+
     text = update.message.text
-    update.message.reply_text(text)
-    # chat_id = update.message.chat_id
-    # context.bot.send_message(
-    #     chat_id=chat_id,
-    #     text=text
-    # )
+    chat_id = update.message.chat_id
+    context.bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        reply_markup=reply_markup
+    )
+
+
+def tg_send_random_question(update: Update, context: CallbackContext):
+    tg_chat_id = update.message.chat_id
+
+    questions = r.keys()
+    random_question = (random.choice(questions)).decode('utf-8')
+
+    while r.get(f'{tg_chat_id}_last_question') is random_question:
+        random_question = (random.choice(questions)).decode('utf-8')
+
+    context.bot.send_message(
+        chat_id=tg_chat_id,
+        text=random_question,
+        reply_markup=reply_markup
+    )
+
+    r.set(f'{tg_chat_id}_last_question', random_question)
+
+
+def start_bot(tg_token):
+    updater = Updater(tg_token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text('Новый вопрос') & ~Filters.command,
+        tg_send_random_question,
+        pass_user_data=True
+    )
+    )
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command,
+        tg_send_answer
+    )
+    )
+
+    updater.start_polling()
+    updater.idle()
 
 
 def main():
+    pass
+
+    # questions_from_db = r.keys()
+    # dispatcher.bot_data['questions'] = questions_from_db
+
+
+if __name__ == '__main__':
     env = Env()
     env.read_env()
 
     tg_token = env.str('TG_TOKEN')
 
+    redis_host = env.str('REDIS_DB_NAME')
+    redis_port = env.int('REDIS_PORT')
+    redis_pass = env.str('REDIS_PASSWORD')
+
+    r = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_pass,
+        db=0
+    )
+
     tg_bot = telegram.Bot(token=tg_token)
     logger.setLevel(logging.WARNING)
     # logger.addHandler(log_handler.TelegramLogsHandler(tg_bot))
 
-    updater = Updater(tg_token)
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command,
-        tg_send_answer
-        )
-    )
-
-    try:
-        updater.start_polling()
-        updater.idle()
-    except requests.exceptions.HTTPError as error:
-        logger.warning('Проблема с телеграм ботом ')
-
-
-if __name__ == '__main__':
-    main()
+    start_bot(tg_token)
