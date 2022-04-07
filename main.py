429e5209f17ff907199790d74298179ff9673ce0
Max Kutovoy <maxkutovoy@yandex.ru>
2022-04-07 11:39:21 +0600

    Parse new questions from file to db.

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
+import redis
+from environs import Env
+
+
+def open_file(file_path, encoding='UTF-8'):
+    with open(file_path, 'r', encoding=encoding) as file:
+        text = file.read()
+    return text
+
+
+def split_note(note):
+    separated_note = note.split('\n\n')
+    for note_part in separated_note:
+        if note_part.startswith('Вопрос'):
+            _, question_text = note_part.split('\n', maxsplit=1)
+        elif note_part.startswith('Ответ'):
+            _, answer_text = note_part.split('\n', maxsplit=1)
+            return question_text, answer_text
 
 
 def main():
-    with open('questions/1vs1200.txt', 'r', encoding='KOI8-R') as file:
-        text = file.read()
-        questions_list = text.split("\n\n\n")
-        
-    questions = {}
-    answers = {}
+    env = Env()
+    env.read_env()
 
-    for entry in questions_list:
-        print(entry)
-        entry = entry.split('\n\n')
-        for note in entry:
+    redis_host = env.str('REDIS_DB_NAME')
+    redis_port = env.int('REDIS_PORT')
+    redis_pass = env.str('REDIS_PASSWORD')
 
-            if note.startswith('Вопрос'):
-                question_number, question_text = note.split('\n', maxsplit=1)
-            elif note.startswith('Ответ'):
-                answer_number, answer_text = note.split('\n', maxsplit=1)
-                questions[question_number] = {
-                    'question': question_text,
-                    'answer': answer_text
-                }
+    r = redis.Redis(
+        host=redis_host,
+        port=redis_port,
+        password=redis_pass,
+        db=0
+    )
+
+    file_path = 'questions/1vs1201.txt'
+    new_questions = open_file(file_path, encoding='KOI8-R').split("\n\n\n")
+    for note in new_questions:
+        question_text, answer_text = split_note(note)
+        r.set(question_text, answer_text)
 
 
 if __name__ == "__main__":
